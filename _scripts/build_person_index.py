cb66d229e4cf8da8199b0e11b199a25b659a7317
Tim Green <timothy.green@gmail.com>
2015-03-23 20:46:54 +0000

    Person index builder now builds into folders

--- _scripts/build_person_index.py
+++ _scripts/build_person_index.py
@@ -5,9 +5,6 @@ ynmp_export_path = sys.argv[1]
 
 ynmp_export = json.load(open(ynmp_export_path))
 
-people_id = {}
-people_constituency_2015 = {}
-
 for person in ynmp_export['persons']:
     # Remove stuff we don't need
     del person['versions']
@@ -29,15 +26,10 @@ for person in ynmp_export['persons']:
     del person['standing_in']
     del person['party_memberships']
 
-    people_id[person['id']] = person
-
-    if 'ge2015' in person['candidacies']:
-        constituency_id = person['candidacies']['ge2015']['constituency']['post_id']
-        if constituency_id not in people_constituency_2015:
-            people_constituency_2015[constituency_id] = []
+    #if 'ge2015' in person['candidacies']:
+    #    constituency_id = person['candidacies']['ge2015']['constituency']['post_id']
         
-        people_constituency_2015[constituency_id].append(person['id'])
+    json.dump(person, open('_data/people/id/{}.json'.format(person['id']), 'w+'), indent=4, sort_keys=True)
 
-json.dump(people_id, open('_data/people_id_index.json', 'w+'), indent=4, sort_keys=True)
-json.dump(people_constituency_2015, open('_data/people_constituency_2015_index.json', 'w+'), indent=4, sort_keys=True)
+#json.dump(people_constituency_2015, open('_data/people_constituency_2015_index.json', 'w+'), indent=4, sort_keys=True)
 
