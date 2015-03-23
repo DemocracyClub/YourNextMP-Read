54e8b7710689ab86aa7b88d5e3c3f903e4ab58e4
Tim Green <timothy.green@gmail.com>
2015-03-23 22:23:20 +0000

    use downloads folder for ynmp data

--- _scripts/get_ynmp.sh
+++ _scripts/get_ynmp.sh
@@ -1,2 +1,2 @@
-curl "https://yournextmp.popit.mysociety.org/api/v0.1/export.json" -o _data/ynmp.popit.mysociety.org.export.json
+curl "https://yournextmp.popit.mysociety.org/api/v0.1/export.json" -o _downloads/ynmp.popit.mysociety.org.export.json
 
