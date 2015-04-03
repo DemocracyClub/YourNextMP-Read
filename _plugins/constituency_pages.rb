3451a3391e19a2383c9a6be42614cd9657d15633
Sym Roe <sym.roe@talusdesign.co.uk>
2015-04-03 10:23:11 +0100

    Improve constutiency page builds

--- _plugins/constituency_pages.rb
+++ _plugins/constituency_pages.rb
@@ -15,7 +15,25 @@ module Jekyll
 
     end
   end
-
+  
+  class ConstituencySlugPage < Page
+    
+    def initialize(site, base, dir, constituency)
+      @site = site
+      @base = base
+      @dir = dir
+      
+      puts constituency['mapit']['name']
+      @name = 'index.html'
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), 'constituency.html')
+      self.data['constituency'] = constituency
+      self.data['title'] = "Candidates (PPCs) for #{constituency['name']} in the UK 2015 General Election"
+      self.data['hero'] = "Candidates for #{constituency['name']}"
+    end
+  end
+  
+  
   class ConstituencyPageGenerator < Generator
     safe true
 
@@ -23,8 +41,13 @@ module Jekyll
       if site.layouts.key? 'constituency'
         dir = 'constituencies/'
         site.data['constituencies']['id'].each_with_index do |constituency, index|
-          puts constituency[0]
+
+          # Make the page with just the ID
           site.pages << ConstituencyPage.new(site, site.source, File.join(dir, constituency[0]), constituency[1])
+
+          # Make the page with the slug as wel
+          slug = Utils.slugify(constituency[1]['mapit']['name'])
+          site.pages << ConstituencyPage.new(site, site.source, File.join(dir, constituency[0], slug), constituency[1])
         end
       end
     end
--- constituencies.md
+++ constituencies.md
@@ -4,8 +4,10 @@ hero: "Constituencies"
 ---
 <div class="home">
 {% for constituency_hash in site.data.constituencies.id %}
-  {% assign mapit_id = constituency_hash[1][0] %}
-  {% assign constituency = constituency_hash[1][0] %}
-  <li><a href="/constituencies/{{mapit_id}}">{{constituency.name}}</a></li>
+  {% assign mapit_id = constituency_hash[0] %}
+  {% assign constituency = constituency_hash[1] %}
+  <li><a href="/constituencies/{{mapit_id}}/{{constituency['mapit']['name']| slugify}}">
+      {{constituency['mapit']['name'] }}
+  </a></li>
 {% endfor %}
 </div>
