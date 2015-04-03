d58c8df594f8b96d86d4f2e4b01d2e040a949604
Sym Roe <sym.roe@talusdesign.co.uk>
2015-04-03 17:46:05 +0100

    Add CVs and clean up build script

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -9,10 +9,16 @@ if [ ! -f _downloads/ynmp.popit.mysociety.org.export.json ]; then
     ./_scripts/get_ynmp.sh
 fi
 
+if [ ! -f _downloads/cv.export.json ]; then
+    ./_scripts/get_cv.sh
+fi
 
+data_files=(
+    _downloads/ynmp.popit.mysociety.org.export.json
+    _data/mapit-WMC-generation-22.json
+    _downloads/electionmentions.export.json
+    _downloads/cv.export.json
+)
 
-python _scripts/build_person_index.py _downloads/ynmp.popit.mysociety.org.export.json
-python _scripts/build_constituency_index.py _downloads/ynmp.popit.mysociety.org.export.json \
-                                            _data/mapit-WMC-generation-22.json \
-                                            _downloads/electionmentions.export.json
-
+python _scripts/build_person_index.py "${data_files[@]}"
+python _scripts/build_constituency_index.py "${data_files[@]}"
--- _scripts/build_constituency_index.py
+++ _scripts/build_constituency_index.py
@@ -4,8 +4,11 @@ import sys
 ynmp_export_path = sys.argv[1]
 mapit_export_path = sys.argv[2]
 em_export_path = sys.argv[3]
+cv_export_path = sys.argv[4]
 
 mapit_export_path = json.load(open(mapit_export_path))
+
+PERSON_CONSTITUENCIES = {}
 
 # Build constituency data, starting with mapit
 constituency_data = {constituency_id: {'mapit': mapit_data,
@@ -33,12 +36,13 @@ def build_person_candidacies(person):
     return candidacies
 
 for person in ynmp_export['persons']:
+    
     candidacies = build_person_candidacies(person)
 
     if 'ge2015' in candidacies:
         constituency_id = candidacies['ge2015']['constituency']['post_id']
-   
         constituency_data[constituency_id]['ynmp'].append(person['id'])
+        PERSON_CONSTITUENCIES[person['id']] = constituency_id
 
 # Build EM data
 election_mentions_export = json.load(open(em_export_path))
@@ -46,6 +50,20 @@ election_mentions_export = json.load(open(em_export_path))
 for constituency_id, links in election_mentions_export.items():
    constituency_data[constituency_id]['em'] = links 
  
+
+# Build CV data
+cv_export = json.load(open(cv_export_path))
+
+for cv in cv_export:
+    cv['person_id'] = str(cv['person_id'])
+    constituency_id = PERSON_CONSTITUENCIES[str(cv['person_id'])]
+    cv_list = constituency_data[constituency_id].get('cv', [])
+    cv_list.append(cv)
+    constituency_data[constituency_id]['cv'] = cv_list
+
+
+
+
 for constituency_id, data in constituency_data.items(): 
     json.dump(data,
               open('_data/constituencies/id/{}.json'.format(constituency_id), 'w+'),
