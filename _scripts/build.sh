1cb1b4f153ca34c772a3ebf77cba1b889f898991
Sym Roe <sym.roe@talusdesign.co.uk>
2015-04-03 16:26:31 +0100

    Download data if it doesn't exist

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+
+if [ ! -f _downloads/electionmentions.export.json ]; then
+    ./_scripts/get_em.sh
+fi
+
+if [ ! -f _downloads/ynmp.popit.mysociety.org.export.json ]; then
+    ./_scripts/get_ynmp.sh
+fi
+
+
+
 python _scripts/build_person_index.py _downloads/ynmp.popit.mysociety.org.export.json
 python _scripts/build_constituency_index.py _downloads/ynmp.popit.mysociety.org.export.json \
                                             _data/mapit-WMC-generation-22.json \
