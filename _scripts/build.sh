09bdd3fdd5e9d8aee20de3b8c96a2532e149b496
Sym Roe <sym.roe@talusdesign.co.uk>
2015-04-04 17:03:42 +0100

    Add EL

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -17,12 +17,17 @@ if [ ! -f _downloads/meet.export.json ]; then
     ./_scripts/get_meet.sh
 fi
 
+if [ ! -f _downloads/el.export.json ]; then
+    ./_scripts/get_el.sh
+fi
+
 data_files=(
     _downloads/ynmp.popit.mysociety.org.export.json
     _data/mapit-WMC-generation-22.json
     _downloads/electionmentions.export.json
     _downloads/cv.export.json
     _downloads/meet.export.json
+    _downloads/el.export.json
 )
 
 python _scripts/build_person_index.py "${data_files[@]}"
---
+++ _scripts/get_el.sh
@@ -0,0 +1,1 @@
+curl "https://electionleaflets.org/api/latest_by_constituency?format=json" -o _downloads/el.export.json
