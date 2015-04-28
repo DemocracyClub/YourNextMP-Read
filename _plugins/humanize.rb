DemocracyClub/YourNextMP-Read

commit 8865fabe6ab008407deb682dc35492d4216b7115

Add a couple of handy grammar tools to humanize

_plugins/humanize.rb
@@ -233,6 +233,19 @@ module Jekyll
 
     end
 
+    def possessive(str)
+      str + ('s' == str[-1,1] ? "'" : "'s")
+    end
+
+    def pronoun(gender, type="subject")
+      pronouns = {
+        male: {subject: "he", object: "him", possessive: "his"},
+        female: {subject: "she", object: "her", possessive: "her"}
+      }
+      pronouns.default = {subject: "they", object: "them", possessive: "their"}
+      return pronouns[gender.downcase.to_sym][type.to_sym]
+    end
+
     #####################
     #  PRIVATE METHODS  #
     #####################

_layouts/constituency.html
@@ -182,7 +182,7 @@ layout: default
   <div class="mentions-block data-block">
     <h3 id="press">Press mentions for {{ page.constituency.mapit.name }}</h3>
 
-    <p class="info">The national and local press say lots of interesting things about your constituency and candidates, so <a href="https://www.electionmentions.com">Election Mentions</a> is collecting news articles about {{ page.constituency.mapit.name }} constituency or the candidates standing here. There may be errors.</p>
+    <p class="info">The national and local press say lots of interesting things about your constituency and candidates, so <a href="https://www.electionmentions.com">Election Mentions</a> is collecting news articles about the {{ page.constituency.mapit.name }} constituency or the candidates standing here. There may be errors.</p>
 
     <ul class="mentions-list">
       {% for mention in page.constituency.em %}

_layouts/person.html
@@ -1,43 +1,14 @@
 ---
 layout: default
 ---
-{% if page.person.gender == 'male' %}
-  {% assign possessive='His' %}
-{% elsif page.person.gender == 'female' %}
-  {% assign possessive='Her' %}
-{% else %}
-  {% assign possessive='Their' %}
-{% endif %}
-
-{% if page.person.gender == 'male' %}
-  {% assign pronoun='Him' %}
-{% elsif page.person.gender == 'female' %}
-  {% assign pronoun='Her' %}
-{% else %}
-  {% assign pronoun='Them' %}
-{% endif %}
-
 {% assign last_letter_of_name=page.person.name|split:''|last %}
-
-{% if last_letter_of_name == 's' %}
-  {% assign s_end='' %}
-{% else %}
-  {% assign s_end='s' %}
-{% endif %}
 
 <div class='person__details'>
   <p>
     {{ page.person.name }} is running in <a href='/constituency/{{ page.person.candidacies.ge2015.constituency.post_id }}/'>{{ page.person.candidacies.ge2015.constituency.name }}</a> as a {{ page.person.candidacies.ge2015.party.name }} candidate.
 
   {% if page.person.candidacies.ge2010 %}
-    {% if page.person.gender == 'male' %}
-        He
-    {% elsif page.person.gender == 'female' %}
-        She
-    {% else %}
-        They
-    {% endif %} 
-    previously ran in
+    {{ page.person.gender | pronoun:"subject" | capitalize }} previously ran in
     {% if page.person.candidacies.ge2010.constituency.post_id == page.person.candidacies.ge2015.constituency.post_id %}
         the same constituency
     {% else %}
@@ -76,7 +47,7 @@ layout: default
   {% endif %}
   
   {% if page.person.links.homepage %}
-  <a class="button" rel="nofollow" href="{{ page.person.links.homepage.url }}"><i class="fa fa-home"></i> {{ page.person.name }}'{{ s_end }} website</a>
+  <a class="button" rel="nofollow" href="{{ page.person.links.homepage.url }}"><i class="fa fa-home"></i> {{ page.person.name|possessive }} website</a>
   {% endif %}
   
   {% if page.person.links.linkedin %}
@@ -130,7 +101,7 @@ layout: default
     </a>
 
     <p class='person__cv__text'>
-        {{ page.person.name}} has sent us {{ possessive|downcase }} CV. 
+        {{ page.person.name}} has sent us {{ page.person.gender|pronoun:"possessive" }} CV.
 
         <a href="http://cv.democracyclub.org.uk/show_cv/{{ page.person.id }}">
         Read {{ page.person.name}}'s CV now.
@@ -138,7 +109,7 @@ layout: default
     </p>
     <div style='clear:both;'></div>
   {% else %}
-    <p class='person__cv__text'>{{ page.person.name }} hasn't sent us {{ possessive|downcase }} CV.</p>
+    <p class='person__cv__text'>{{ page.person.name }} hasn't sent us {{ page.person.gender|pronoun:"possessive" }} CV.</p>
 
     <a class='button' href='http://cv.democracyclub.org.uk/upload_cv/{{ page.person.id }}'>Ask {{ pronoun|downcase }} now</a>
   {% endif %}

commit 2ea8f7c48195ef124ec8304e72f4dbc82f702859

Add a check for Nil gender
Fixes bug introduced in 8865fabe

_plugins/humanize.rb
@@ -238,12 +238,15 @@ module Jekyll
     end
 
     def pronoun(gender, type="subject")
+      gender = if gender then gender.strip.downcase else "" end
+
       pronouns = {
         male: {subject: "he", object: "him", possessive: "his"},
         female: {subject: "she", object: "her", possessive: "her"}
       }
       pronouns.default = {subject: "they", object: "them", possessive: "their"}
-      return pronouns[gender.downcase.to_sym][type.to_sym]
+
+      return pronouns[gender.to_sym][type.to_sym]
     end
 
     #####################