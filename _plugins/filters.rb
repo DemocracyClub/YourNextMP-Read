aca274f0f16b79fbf570fbe4a724b4331d6a621c
Tim Green <timothy.green@gmail.com>
2015-05-05 20:05:48 +0000

    make more like ynmp-write branch. add proxy url for OG.

--- _plugins/filters.rb
+++ _plugins/filters.rb
@@ -4,6 +4,23 @@ module Jekyll
       '%.1f' % number
     end
   end
+
+  module PopitFilters
+    def fixproxyurl(value)
+        if not value
+            return ''
+        end
+
+        # Some PopIt versions have a double-slash accidentally included:
+        result = value.sub('image-proxy//', 'image-proxy/')
+
+        # Force HTTPS in images
+        result = result.sub('^http://', 'https://')
+
+        return result
+    end
+  end
 end
 
 Liquid::Template.register_filter(Jekyll::NumberFilters)
+Liquid::Template.register_filter(Jekyll::PopitFilters)
